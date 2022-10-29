efad8d443e578e34be493942cf5e83d76c5c768e
Zach <z.d.jorgensen@gmail.com>
2022-10-29 00:42:45 -0600

    thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -23,11 +23,11 @@ module.exports = {
     // Creates a new thought
     createThought(req, res) {
         Thought.create(req.body)
-        .then((thought) => res.json(thought))
-        .catch((err) => {
-            console.log(err)
-            return res.status(500).json(err)
-        })
+            .then((thought) => res.json(thought))
+            .catch((err) => {
+                console.log(err)
+                return res.status(500).json(err)
+            })
     },
 
     // Deletes a single thought
@@ -37,45 +37,20 @@ module.exports = {
             { $pull: { thoughtId: req.params.thoughtId } },
         )
         res.json(deletedThought)
-        .catch((err) =>
-            res.status(500).json(err.message));
+            .catch((err) =>
+                res.status(500).json(err.message));
     },
 
     // Updates a single thought
     updateThought(req, res) {
         const updatedThought = Thought.findByIdAndUpdate(
             { _id: req.params.thoughtId },
-            { $set: {thoughtText: req.body }}
+            { $set: { thoughtText: req.body } }
         )
         res.json(updatedThought)
-        .catch((err) => 
-            res.status(500).json(err.message));
-    }
-}
+            .catch((err) =>
+                res.status(500).json(err.message))
         .then((thoughts) => res.json(thoughts))
-        .catch((err) => res.status(500).json(err));
-    },
-    // Gets a thought by the Id
-    getThoughtById(req, res) {
-        Thought.findOne({ _id: req.params.thoughtId })
-        .select('-__v')
-        .then((thoughts) =>
-            !thoughts
-                ? res.status(404).json({ message: 'No thought '})
-                : res.json(thoughts)
-        )
-        .catch((err) => res.status(500).json(err));
-    },
-    // Creates a new thought
-    createThought (req, res ) {
-
-    },
-    // Deletes a single thought
-    deleteThought (req, res ) {
-
-    },
-    // Updates a single thought
-    updateThought (req, res ) {
-
+        .catch((err) => res.status(500).json(err))
     },
 }
